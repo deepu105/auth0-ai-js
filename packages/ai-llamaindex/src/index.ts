6b7539a569455065c52c8c3a62f771592d90ddc8
Jared Hanson <jaredhanson@gmail.com>
2024-10-17 14:03:18 -0700

    Stub out loop in ai-llamaindex.

--- packages/ai-llamaindex/src/index.ts
+++ packages/ai-llamaindex/src/index.ts
@@ -1,3 +1,14 @@
+import { agentAsyncStorage } from '../../ai'; // '@auth0/ai';
+
 export function loop(agent, params, ctx) {
-  console.log('loop...');
+  return agentAsyncStorage.run(ctx, async () => {
+    try {
+      const response = await agent.chat(params);
+      return response;
+    } catch (ex) {
+      console.log('???')
+      console.log(ex)
+      console.log(ex.scope);
+    }
+  });
 }
