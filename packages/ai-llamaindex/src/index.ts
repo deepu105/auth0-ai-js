19045b2ef03e8c1a8c8ea3cbe8416f8d80cd1c2c
Jared Hanson <jaredhanson@gmail.com>
2024-10-21 12:54:30 -0700

    Save history and request authorization in @auth0/ai-llamaindex.

--- packages/ai-llamaindex/src/index.ts
+++ packages/ai-llamaindex/src/index.ts
@@ -1,4 +1,4 @@
-import { agentAsyncStorage } from '../../ai'; // '@auth0/ai';
+import { CIBAAuthorizer, FSStore, AuthorizationError, agentAsyncStorage } from '../../ai'; // '@auth0/ai';
 
 export function loop(agent, params, ctx) {
   return agentAsyncStorage.run(ctx || {}, async () => {
@@ -9,6 +9,23 @@ export function loop(agent, params, ctx) {
       console.log('???')
       console.log(ex)
       console.log(ex.scope);
+      
+      // NOTE: requires modifications to @llamaindex/core/agent/dist/index.js:L186, callTool
+      // NOTE: requires modifications to @llamaindex/core/agent/dist/index.cjs:L188, callTool
+      if (ex instanceof AuthorizationError) {
+        var authorizer = new CIBAAuthorizer('http://localhost:3000/ciba/bc-authorize');
+        var tid = await authorizer.authorize('stock.buy');
+        
+        // Slice off the last message, under the assumption that it was a tool call that failed
+        // TODO: make this more robust by checking
+        var messages = agent.chatHistory.slice(0, -1);
+        
+        var store = new FSStore('.');
+        await store.store(tid, messages);
+        return;
+      }
+      
+      // TODO: Feed other errors back into agent
     }
   });
 }
