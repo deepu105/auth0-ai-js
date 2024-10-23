8c4b35b6dceff6fb3c3e09e75656a90bd7d9361c
Jared Hanson <jaredhanson@gmail.com>
2024-10-23 11:48:07 -0700

    Stub out genkit example.

---
+++ examples/genkit/trader/agent.ts
@@ -0,0 +1,40 @@
+// NOTE: this throws errors that were fixed by commenting out unknown symbols
+// in node_modules/@genkit-ai/ai/lib/index.mjs
+import { generate, defineTool } from '@genkit-ai/ai';
+import { configureGenkit } from '@genkit-ai/core';
+import { openAI, gpt4o } from 'genkitx-openai';
+import * as z from 'zod';
+import { buy } from './tools/buy';
+
+import 'dotenv/config'
+
+configureGenkit({
+  plugins: [
+    openAI()
+  ],
+  // Log debug output to tbe console.
+  //logLevel: 'debug',
+  // Perform OpenTelemetry instrumentation and enable trace collection.
+  enableTracingAndMetrics: true,
+});
+
+const buyToolInputSchema = z.object({ ticker: z.string(), qty: z.number() });
+const buyTool = defineTool(
+  {
+    name: "buy",
+    description: "Use this function to buy stock",
+    inputSchema: buyToolInputSchema,
+    outputSchema: z.string(),
+  },
+  buy
+);
+
+
+const result = await generate({
+  model: gpt4o,
+  prompt: 'Buy 100 shares of ZEKO',
+  tools: [buyTool]
+});
+
+console.log(result);
+console.log(result.text())
---
+++ examples/genkit/trader/tools/buy.ts
@@ -0,0 +1,19 @@
+//import { agentAsyncStorage } from '@auth0/ai';
+//import { AuthorizationError } from '@auth0/ai';
+
+export let buy = async function({ ticker, qty }) {
+  console.log('buy stock!');
+  console.log(ticker);
+  console.log(qty)
+  //console.log('BUY: ' + qty + ' ' + ticker);
+  
+  /*
+  var store = agentAsyncStorage.getStore();
+  console.log(store);
+  if (!store.token) {
+    throw new AuthorizationError('You need authorization to buy stock', 'insufficient_scope', 'stock.buy');
+  }
+  */
+  
+  return 'OK'
+}
