37118e829c02c07fe31a8d5631ee63dfd101fc70
Jared Hanson <jaredhanson@gmail.com>
2024-10-24 10:52:05 -0700

    Update genkit example to handle resume.

--- examples/genkit/trader/agent.ts
+++ examples/genkit/trader/agent.ts
@@ -1,10 +1,11 @@
+import { defineCommand, runMain } from 'citty';
 // NOTE: this throws errors that were fixed by commenting out unknown symbols
 // in node_modules/@genkit-ai/ai/lib/index.mjs
 import { generate, defineTool } from '@genkit-ai/ai';
 import { configureGenkit } from '@genkit-ai/core';
 import { openAI, gpt4o } from 'genkitx-openai';
 import * as z from 'zod';
-import { loop } from '@auth0/ai-genkit';
+import { loop, reenterLoop } from '@auth0/ai-genkit';
 import { buy } from './tools/buy';
 
 import 'dotenv/config'
@@ -30,26 +31,74 @@ const buyTool = defineTool(
   buy
 );
 
-
-/*
-const result = await generate({
-  model: gpt4o,
-  prompt: 'Buy 100 shares of ZEKO',
-  tools: [buyTool]
+const main = defineCommand({
+  meta: {
+    name: "trader",
+    version: "0.0.0",
+    description: "An example AI trader with human approval",
+  },
+  args: {
+    message: {
+      type: "positional",
+      description: "A message to the agent",
+      required: false,
+    },
+    token: {
+      alias: "C",
+      type: "string",
+      description: "Access token for taking actions",
+    },
+    thread: {
+      alias: "t",
+      type: "string",
+      description: "Thread to continue",
+    },
+  },
+  run({ args }) {
+    if (args.thread) {
+      resume(gpt4o, [ buyTool ], args.thread, args.token);
+      return;
+    }
+    
+    prompt(gpt4o, [ buyTool], args.message);
+    
+    /*
+    const agent = new OpenAIAgent({
+      tools: [ buyTool ],
+      verbose: true
+    });
+    
+    
+    if (args.thread) {
+      resume(agent, args.thread, args.token);
+      return;
+    }
+    
+    prompt(agent, args.message);
+    */
+  },
 });
 
-console.log(result);
-console.log(result.text())
-*/
+runMain(main);
 
-const response = await loop(generate, {
-  model: gpt4o,
-  prompt: 'Buy 100 shares of ZEKO',
-  tools: [buyTool]
-});
+async function resume(model, tools, threadID, token) {
+  console.log('resume...');
+  
+  const result = await reenterLoop(generate, { model: model, tools: tools }, threadID, { token: token });
+  if (result) {
+    console.log(result.text())
+  }
+}
 
-console.log('=== DONE ===');
- 
-if (response) {
-  console.log(response.text())
+async function prompt(model, tools, message) {
+  const result = await loop(generate, {
+    model: model,
+    prompt: message,
+    tools: tools
+  });
+
+  //console.log('=== DONE ===');
+  if (result) {
+    console.log(result.text())
+  }
 }
