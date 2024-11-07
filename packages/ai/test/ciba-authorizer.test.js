f044a79571a7f7824b009c69deb49e8fcdf15600
Jared Hanson <jaredhanson@gmail.com>
2024-11-07 10:38:54 -0800

    Clean up tests.

--- packages/ai/test/ciba-authorizer.test.js
+++ packages/ai/test/ciba-authorizer.test.js
@@ -3,51 +3,66 @@ import { CIBAAuthorizer } from '../src/ciba-authorizer'
 
 describe('CIBAAuthorizer#authorize', () => {
   
-  it('should request authorization with scope', async () => {
-    vi.stubGlobal('fetch', vi.fn(() => {
-      return Promise.resolve({
-        ok: true,
-        json: () => {
-          return Promise.resolve({ auth_req_id: '1c266114-a1be-4252-8ad1-04986c5b9ac1' });
-        }
-      })
-    }))
+  describe('constructor', async () => {
     
-    const authorizer = new CIBAAuthorizer('http://example.test/bc-authorize')
+    it('should construct with client secret', () => {
+      const authorizer = new CIBAAuthorizer({ url: 'http://example.test/bc-authorize', clientID: 's6BhdRkqt3', clientSecret: '7Fjfp0ZBr1KtDRbnfVdmIw' })
+      expect(authorizer.url).toBe('http://example.test/bc-authorize')
+      expect(authorizer.clientID).toBe('s6BhdRkqt3')
+      expect(authorizer.clientSecret).toBe('7Fjfp0ZBr1KtDRbnfVdmIw')
+    })
     
-    const x = await authorizer.authorize({ scope: [ 'urn:example:channel=HBO', 'urn:example:rating=G,PG-13' ] })
-    expect(fetch).toHaveBeenCalledWith('http://example.test/bc-authorize', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: 'scope=urn%3Aexample%3Achannel%3DHBO+urn%3Aexample%3Arating%3DG%2CPG-13'
-    });
-    expect(x).toEqual('1c266114-a1be-4252-8ad1-04986c5b9ac1')
-  }) // should request authorization with scope
+  })
   
-  it('should request authorization with scope and single ACR value', async () => {
-    vi.stubGlobal('fetch', vi.fn(() => {
-      return Promise.resolve({
-        ok: true,
-        json: () => {
-          return Promise.resolve({ auth_req_id: '1c266114-a1be-4252-8ad1-04986c5b9ac1' });
-        }
-      })
-    }))
+  describe('#authorize', async () => {
     
-    const authorizer = new CIBAAuthorizer('http://example.test/bc-authorize')
+    it('should request authorization with ACR value', async () => {
+      vi.stubGlobal('fetch', vi.fn(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => {
+            return Promise.resolve({ auth_req_id: '1c266114-a1be-4252-8ad1-04986c5b9ac1' });
+          }
+        })
+      }))
     
-    const x = await authorizer.authorize({ acrValues: [ 'myACR' ], scope: [ 'purchase' ] })
-    expect(fetch).toHaveBeenCalledWith('http://example.test/bc-authorize', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: 'acr_values=myACR&scope=purchase'
-    });
-    expect(x).toEqual('1c266114-a1be-4252-8ad1-04986c5b9ac1')
-  }) // should request authorization with scope and single ACR value
+      const authorizer = new CIBAAuthorizer('http://example.test/bc-authorize')
+      const x = await authorizer.authorize({ scope: [ 'openid' ], acrValues: [ 'myACR' ] })
+      expect(fetch).toHaveBeenCalledWith('http://example.test/bc-authorize', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'acr_values=myACR&scope=openid'
+      });
+      expect(x).toEqual('1c266114-a1be-4252-8ad1-04986c5b9ac1')
+    }) // should request authorization with ACR value
+    
+    it('should request authorization with multiple scopes', async () => {
+      vi.stubGlobal('fetch', vi.fn(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => {
+            return Promise.resolve({ auth_req_id: '1c266114-a1be-4252-8ad1-04986c5b9ac1' });
+          }
+        })
+      }))
+    
+      const authorizer = new CIBAAuthorizer('http://example.test/bc-authorize')
+      const x = await authorizer.authorize({ scope: [ 'urn:example:channel=HBO', 'urn:example:rating=G,PG-13' ] })
+      expect(fetch).toHaveBeenCalledWith('http://example.test/bc-authorize', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'scope=urn%3Aexample%3Achannel%3DHBO+urn%3Aexample%3Arating%3DG%2CPG-13'
+      });
+      expect(x).toEqual('1c266114-a1be-4252-8ad1-04986c5b9ac1')
+    }) // should request authorization with multiple scopes
+    
+  }) // #authorize
+  
+  
   
   it('should authenticate with client secret using basic authentication scheme', async () => {
     vi.stubGlobal('fetch', vi.fn(() => {
