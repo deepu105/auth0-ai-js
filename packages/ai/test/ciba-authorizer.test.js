215d796849b98d2de8fb20e0aeb967f758851f0a
Jared Hanson <jaredhanson@gmail.com>
2024-11-07 10:40:59 -0800

    Clean up tests.

--- packages/ai/test/ciba-authorizer.test.js
+++ packages/ai/test/ciba-authorizer.test.js
@@ -60,32 +60,29 @@ describe('CIBAAuthorizer#authorize', () => {
       expect(x).toEqual('1c266114-a1be-4252-8ad1-04986c5b9ac1')
     }) // should request authorization with multiple scopes
     
+    it('should authenticate using HTTP basic scheme', async () => {
+      vi.stubGlobal('fetch', vi.fn(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => {
+            return Promise.resolve({ auth_req_id: '1c266114-a1be-4252-8ad1-04986c5b9ac1' });
+          }
+        })
+      }))
+    
+      const authorizer = new CIBAAuthorizer({ url: 'http://example.test/bc-authorize', clientID: 's6BhdRkqt3', clientSecret: '7Fjfp0ZBr1KtDRbnfVdmIw' })
+      const x = await authorizer.authorize({ scope: [ 'openid' ], acrValues: [ 'myACR' ] })
+      expect(fetch).toHaveBeenCalledWith('http://example.test/bc-authorize', {
+        method: 'POST',
+        headers: {
+          'Authorization': 'Basic czZCaGRSa3F0Mzo3RmpmcDBaQnIxS3REUmJuZlZkbUl3',
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'acr_values=myACR&scope=openid'
+      });
+      expect(x).toEqual('1c266114-a1be-4252-8ad1-04986c5b9ac1')
+    }) // should authenticate using HTTP basic scheme
+    
   }) // #authorize
-  
-  
-  
-  it('should authenticate with client secret using basic authentication scheme', async () => {
-    vi.stubGlobal('fetch', vi.fn(() => {
-      return Promise.resolve({
-        ok: true,
-        json: () => {
-          return Promise.resolve({ auth_req_id: '1c266114-a1be-4252-8ad1-04986c5b9ac1' });
-        }
-      })
-    }))
-    
-    const authorizer = new CIBAAuthorizer({ url: 'http://example.test/bc-authorize', clientID: 's6BhdRkqt3', clientSecret: '7Fjfp0ZBr1KtDRbnfVdmIw' })
-    
-    const x = await authorizer.authorize({ scope: [ 'urn:example:channel=HBO', 'urn:example:rating=G,PG-13' ] })
-    expect(fetch).toHaveBeenCalledWith('http://example.test/bc-authorize', {
-      method: 'POST',
-      headers: {
-        'Authorization': 'Basic czZCaGRSa3F0Mzo3RmpmcDBaQnIxS3REUmJuZlZkbUl3',
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: 'scope=urn%3Aexample%3Achannel%3DHBO+urn%3Aexample%3Arating%3DG%2CPG-13'
-    });
-    expect(x).toEqual('1c266114-a1be-4252-8ad1-04986c5b9ac1')
-  }) // should authenticate with client secret using basic authentication scheme
   
 })
