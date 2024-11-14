ea24397bf35bad3b48c72fbe0d5c94031d25eac1
Jared Hanson <jaredhanson@gmail.com>
2024-11-14 15:11:00 -0800

    Rethrow errors in interact.

--- packages/ai/src/interact.ts
+++ packages/ai/src/interact.ts
@@ -10,19 +10,20 @@ export function interact(fn, authorizer, receiver) {
       try {
         return await fn.apply(undefined, args);
       } catch (error) {
-        // The function threw an `AuthorizationError`, indicating that the
-        // authentication context is not sufficient.  This error _may_ be
-        // remediable by authenticating the user or obtaining their consent.
-        //
-        // This error is typically the result of an HTTP authentication
-        // challenge received at run-time.  Endpoints that respond with such
-        // challenges are encouraged to use the attributes defined by
-        // [RFC 6750][1] and [RFC 9470][2] so that the necessary authorization
-        // can be obtained by interacting with the user.
-        //
-        // [1]: https://datatracker.ietf.org/doc/html/rfc6750
-        // [2]: https://datatracker.ietf.org/doc/html/rfc9470
         if (error instanceof AuthorizationError) {
+          // The function threw an `AuthorizationError`, indicating that the
+          // authentication context is not sufficient.  This error _may_ be
+          // remediable by authenticating the user or obtaining their consent.
+          //
+          // This error is typically the result of an HTTP authentication
+          // challenge received at run-time.  Endpoints that respond with such
+          // challenges are encouraged to use the attributes defined by
+          // [RFC 6750][1] and [RFC 9470][2] so that the necessary authorization
+          // can be obtained by interacting with the user.
+          //
+          // [1]: https://datatracker.ietf.org/doc/html/rfc6750
+          // [2]: https://datatracker.ietf.org/doc/html/rfc9470
+          
           var params: AuthorizationOptions = {};
           if (store.user) {
             params.loginHint = store.user.id;
@@ -41,10 +42,7 @@ export function interact(fn, authorizer, receiver) {
           return ifn.apply(undefined, arguments);
         }
         
-        // TODO
-        
-        console.log('RETHROW');
-        console.log(error);
+        throw error;
       }
     });
   };
