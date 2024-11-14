05065d44c665a6416b6a880747350e8c2e5e3734
Jared Hanson <jaredhanson@gmail.com>
2024-11-14 13:37:42 -0800

    Add comments.

--- packages/ai/src/interact.ts
+++ packages/ai/src/interact.ts
@@ -12,6 +12,18 @@ export function interact(fn, authorizer) {
       try {
         return await fn.apply(undefined, args);
       } catch (error) {
+        // The function threw an `AuthorizationError`, indicating that the
+        // authentication context is not sufficient.  This error _may_ be
+        // remediable by authenticating the user or obtaining their consent.
+        //
+        // This error is typically the result of an HTTP authentication
+        // challenge received at run-time.  Endpoints that respond with such
+        // challenges are encouraged to use the attributes defined by
+        // [RFC 6750][1] and [RFC 9470][2] so that the necessary authorization
+        // can be obtained by interacting with the user.
+        //
+        // [1]: https://datatracker.ietf.org/doc/html/rfc6750
+        // [2]: https://datatracker.ietf.org/doc/html/rfc9470
         if (error instanceof AuthorizationError) {
           var params: AuthorizationOptions = {};
           if (store.user) {
