fc46c292f79cde2dbe61c56d9b42a16e44d9741f
Jared Hanson <jaredhanson@gmail.com>
2024-11-18 15:32:33 -0800

    Stub out tests.

---
+++ packages/ai-langchain/test/index.test.js
@@ -0,0 +1,8 @@
+// sum.test.js
+import { expect, test } from 'vitest'
+//import { sum } from './sum.js'
+
+test('adds 1 + 2 to equal 3', () => {
+  //expect(sum(1, 2)).toBe(3)
+  expect(1 + 2).toBe(3)
+})
