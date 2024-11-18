4cb34e4deb92d4dc043e5782a09e95913c2412f1
Jared Hanson <jaredhanson@gmail.com>
2024-11-18 10:55:25 -0800

    Further stub out genkit tool calling loop.

--- packages/ai-genkit/src/loop.ts
+++ packages/ai-genkit/src/loop.ts
@@ -2,12 +2,41 @@ export function loop(generate) {
   
   
   const lfn = async function(params) {
-    console.log('genkit loop...');
-    console.log(params);
+    // https://firebase.google.com/docs/genkit/tool-calling
+    // manually handle tool calling, so that we can obtain history and save it as needed
+    params.returnToolRequests = true;
     
-    return await generate(params);
+    let llmResponse;
+    while (true) {
+      llmResponse = await generate(params);
+      const toolRequests = llmResponse.toolRequests();
+      if (toolRequests.length < 1) {
+        break;
+      }
+      //const toolResponses: ToolResponsePart[] = await Promise.all(
+      const toolResponses = await Promise.all(
+        toolRequests.map(async (part) => {
+          const tool = params.tools.find((tool) => tool.__action.name == part.toolRequest.name);
+          if (!tool) {
+            throw Error('Tool not found');
+          }
+          
+          const output = await tool(tool.__action.inputSchema.parse(part.toolRequest?.input));
+          return {
+            toolResponse: {
+              name: part.toolRequest.name,
+              ref: part.toolRequest.ref,
+              output: output,
+            }
+          };
+        })
+      );
+      //params.messages = llmResponse.messages;
+      params.history = llmResponse.toHistory();
+      params.prompt = toolResponses;
+    }
     
-    
+    return llmResponse;
   }
   return lfn;
 }
