0ee292aec4ee239a69008fa903f227380dc6ec15
Jared Hanson <jaredhanson@gmail.com>
2024-11-19 09:16:13 -0800

    Add FS session store.

---
+++ packages/ai-genkit/src/session/fs-session-store.ts
@@ -0,0 +1,26 @@
+import { type SessionStore, type SessionData } from 'genkit';
+import { readFile, writeFile } from 'node:fs/promises';
+
+// https://github.com/firebase/genkit/blob/main/js/ai/src/session.ts
+
+export class FSSessionStore<S = any> implements SessionStore<S> {
+  async get(sessionId: string): Promise<SessionData<S> | undefined> {
+    console.log('GET SESSION: ' + sessionId);
+    
+    try {
+      const s = await readFile(`${sessionId}.json`, { encoding: 'utf8' });
+      const data = JSON.parse(s);
+      return data;
+    } catch {
+      return undefined;
+    }
+  }
+
+  async save(sessionId: string, sessionData: SessionData<S>): Promise<void> {
+    console.log('SAVE SESSION: ' + sessionId);
+    console.log(sessionData);
+    
+    const s = JSON.stringify(sessionData);
+    await writeFile(`${sessionId}.json`, s, { encoding: 'utf8' });
+  }
+}
