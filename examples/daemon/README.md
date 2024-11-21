e146df17b3c90780a8cf45869362b15793bc102e
Jared Hanson <jaredhanson@gmail.com>
2024-11-21 10:10:38 -0800

    Add comments.

--- examples/daemon/README.md
+++ examples/daemon/README.md
@@ -1,2 +1,29 @@
-# CIBA CLI
+# daemon
 
+This example illustrates how to host an agent in an application that runs in the
+background, performing tasks without direct user interaction.
+
+The application can host an agent developed with any of a variety of popular AI
+frameworks.  An example stock trading agent is available using the following
+frameworks:
+
+  - [Genkit]((../agent-genkit))
+  - [LlamaIndex]((../agent-llamaindex))
+
+Because this agent runs in the background, it is unable to directly interact with
+a human for any tasks which require human consent.  Instead, it interacts with
+via out-of-band channels using [OpenID Connect Client-Initiated Backchannel
+Authentication Flow](https://openid.net/specs/openid-client-initiated-backchannel-authentication-core-1_0.html)
+(CIBA).
+
+[`@auth0/ai`](../../packages/ai) orchestrates the interaction between the agent
+and the human, in this case through an instance of `PollingCIBAAuthorizer`.  The
+agent itself is unaware of the modality in which it is running, allowing it to
+be hosted in multiple different contexts - without changes to the agent
+architecture.
+
+## Usage
+
+```sh
+$ npx tsx agent.ts -u "auth0|672d15e3a67830e930d6679b" "Buy 100 shares of ZEKO"
+```
--- examples/daemon/agent.ts
+++ examples/daemon/agent.ts
@@ -18,18 +18,31 @@ const main = defineCommand({
       type: "positional",
       description: "A message to the agent",
       required: false,
+    },
+    username: {
+      alias: "u",
+      type: "string",
+      description: "Identifier for the user",
     }
   },
   async run({ args }) {
+    // Instantiate a CIBA authorizer, which is suitable for obtaining
+    // authorization via out-of-band channels.  Since this daemon runs in the
+    // background, use of direct channels (such as HTTP redirection, common in
+    // "traditional" OAuth flows) is not possible.
     const authorizer = new Auth0PollingCIBAAuthorizer({
       domain: process.env['DOMAIN'],
       clientId: process.env['CLIENT_ID'],
       clientSecret: process.env['CLIENT_SECRET']
     });
     
+    // Wrap the agent's `prompt` method for interaction with the user.  When
+    // the agent encounters an authorization challenge, `interact` will
+    // orchestrate the process of obtaining necessary credentials, including
+    // human-in-the-loop interaction if necessary.
     const interactivePrompt = interact(prompt, authorizer);
     const user = {
-      id: 'auth0|672d15e3a67830e930d6679b'
+      id: args.username
     }
     
     let rv = await interactivePrompt({ user: user }, args.message);
